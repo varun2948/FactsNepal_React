2b3b2e7779d02ca4d4c96b073f2488a8ea0866ab
Varun2948 <deepak.pradhan048@gmail.com>
2020-01-06 14:48:50 +0545

    Project Design Sorting

--- src/components/HomePage/CaseStudy/CaseStudy.js
+++ src/components/HomePage/CaseStudy/CaseStudy.js
@@ -1,91 +1,53 @@
 import axios from "axios";
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import Fade from "react-reveal/Fade";
-
+import CaseStudyData from "./CaseStudyData";
 
 const headerData = {
-    headers: {
-        "Content-Type": "application/json",
-        "X-CSRF-TOKEN": "TuJoRzMGXIzBhy3OH8JJXFmoegsooPSCB5dnw5q4",
-        "X-Requested-With": "XMLHttpRequest"
-    }
+  headers: {
+    "Content-Type": "application/json",
+    "X-CSRF-TOKEN": "TuJoRzMGXIzBhy3OH8JJXFmoegsooPSCB5dnw5q4",
+    "X-Requested-With": "XMLHttpRequest"
+  }
 };
 
 class CaseStudy extends Component {
-    state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      casestudy: []
+    };
+  }
+  componentDidMount() {
+    Promise.all([
+      axios({
+        method: "GET",
+        url: `${process.env.API_URL}api/casestudy`,
+        headers: {
+          "content-type": "application/json",
+          Accept: "application/json"
+        }
+      })
+    ]).then(response => {
+      console.log(response[0].data.collaborators);
+      const sliced2Collaborators = response[0].data.collaborators.slice(0, 2);
+      this.setState({
+        casestudy: sliced2Collaborators
+      });
+      // console.log(this.state.casestudy, "cs");
+    });
+  }
 
-    render() {
-        return (
-            <section className="case-study pdt-127 pdb-142">
-                <div className="container">
-                    <div className="case-study-wrap">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="case-study-image">
-                                    <figure className="figure-case-img">
-                                    <img className="bg-case orange" src="./img/IMAGE-shape2.svg" />
-                                    <img className="bg-case pink" src="./img/IMAGE-shape4.svg" />
-                                    <img className="bg-case sm-pink" src="./img/IMAGE-shape1.svg" />
-                                    <img className="bg-case sm-orange" src="./img/IMAGE-shape3.svg" />
-                                        <img src="./img/bags-best-friends-daylight-1516440.png" alt="" />
-                                    </figure>
-                                </div>
-                            </div>
-                        
-                            <div className="col-md-6">
-                                <div className="case-study-content">
-                                    <div className="case-study-content-wrap">
-                                        <span className="sub-caption">July 2014</span>
-                                        <h3 className="headline2">Student Perceptions of Leadership, Accountability, and Governance of Student Leaders and Unions</h3>
-                                        <p className="body-para">The National Youth Policy has recognized that youth are an invaluable asset to the nation, acknowledging youths’ role as agents of economic, social, and political change.</p>
-                                        <span className="caption">Funded by</span>
-                                        <div className="img-wrap">
-                                            <img className="sub-span-img" src="./img/SFCG_newlogo-960x170.png" alt="Search for Common Ground" />
-                                        </div>
-                                        <button className="btn-case">Read Case Study</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="case-study-wrap pdt-117">
-                        <div className="row">
-                            <div className="order-2 order-sm-2 order-md-1 col-md-6">  
-                                <div className="case-study-content">
-                                <div className="case-study-content-wrap">
-                                    <span className="sub-caption">September 2016</span>
-                                    <h3 className="headline2">Exploring the nature of VAWG, and its effects among migrant labour communities in Baglung district, Nepal</h3>
-                                    <p className="body-para">This presentation shares the preliminary findings from the formative research conducted among communities engaged in migrant labour in Baglung district of Nepal.</p>
-                                    <div className="logo-wrap">
-                                        <div className="logo-mid">
-                                            <img src="./img/vso.png" alt="vso" />
-                                            <img src="./img/south-african-medical-research-council-samrc-vector-logo.png" alt="samrc" />
-                                            <img src="./img/sdd_logo.png" alt="sdd" />
-                                        </div>
-                                    </div>
-                                    <button className="btn-case">Read Case Study</button>
-                                </div>
-                            </div>
-                            </div>
-                            <div className="order-1 order-sm-1 order-md-2 col-md-6">
-                                <div className="case-study-image">
-                                    <figure className="figure-case-img">
-                                        <img className="bg-case orange" src="./img/IMAGE-shape2.svg" />
-                                        <img className="bg-case pink" src="./img/IMAGE-shape4.svg" />
-                                        <img className="bg-case sm-pink" src="./img/IMAGE-shape1.svg" />
-                                        <img className="bg-case sm-orange" src="./img/IMAGE-shape3.svg" />
-                                        <img src="./img/matteo-vegetti-myanmar-06-monastery-kitchen.png" alt="" />
-                                    </figure>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-        );
-    }
+  render() {
+    const { casestudy } = this.state;
+    return (
+      <section className="case-study pdt-132 pdb-128">
+        <div className="container">
+          <CaseStudyData casestudy={casestudy} />
+        </div>
+      </section>
+    );
+  }
 }
 
 export default CaseStudy;
